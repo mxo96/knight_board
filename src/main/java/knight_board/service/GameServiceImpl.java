e11cce9ab1d08d4802e507bfe8b00af79180c638
mirko.caruso <mirko.caruso@jobrapido.com>
2021-07-14 14:50:21 +0200

    catch various exceptions

--- src/main/java/knight_board/service/GameServiceImpl.java
+++ src/main/java/knight_board/service/GameServiceImpl.java
@@ -2,10 +2,15 @@ package knight_board.service;
 
 import static java.lang.String.format;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.SocketTimeoutException;
 import java.net.URL;
 
+import com.fasterxml.jackson.core.JsonParseException;
+import com.fasterxml.jackson.databind.JsonMappingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import knight_board.exceptions.BoardDefinitionException;
@@ -19,7 +24,6 @@ public class GameServiceImpl implements GameService {
     private static final String COMMANDS_API = System.getenv("COMMANDS_API");
     private final ObjectMapper mapper = new ObjectMapper();
 
-    //TODO check if its better to manage the various exception
     @Override
     public Board boardDefinition() throws BoardDefinitionException {
         try {
@@ -29,12 +33,19 @@ public class GameServiceImpl implements GameService {
             final InputStream responseStream = connection.getInputStream();
 
             return mapper.readValue(responseStream, Board.class);
-        } catch (final Exception e) {
+        } catch (final SocketTimeoutException e) {
+            throw new BoardDefinitionException(format("There was a timeout with the Board API [boardApi=%s]", BOARD_API), e);
+        } catch (JsonParseException e) {
+            throw new BoardDefinitionException(format("There was a problem with json parsing for the Board API response [boardApi=%s]", BOARD_API), e);
+        } catch (MalformedURLException e) {
+            throw new BoardDefinitionException(format("There was a problem with the url of the Board API [boardApi=%s]", BOARD_API), e);
+        } catch (JsonMappingException e) {
+            throw new BoardDefinitionException(format("There was a problem with json mapping for the Board API response [boardApi=%s]", BOARD_API), e);
+        } catch (IOException e) {
             throw new BoardDefinitionException(format("There was a problem with the Board API [boardApi=%s]", BOARD_API), e);
         }
     }
 
-    //TODO check if its better to manage the various exception
     @Override
     public Commands commands() throws CommandsException {
         try {
@@ -44,7 +55,15 @@ public class GameServiceImpl implements GameService {
             final InputStream responseStream = connection.getInputStream();
 
             return mapper.readValue(responseStream, Commands.class);
-        } catch (final Exception e) {
+        } catch (final SocketTimeoutException e) {
+            throw new CommandsException(format("There was a timeout with the Commands API [commandsApi=%s]", COMMANDS_API), e);
+        } catch (JsonParseException e) {
+            throw new CommandsException(format("There was a problem with json parsing for the Commands API response [commandsApi=%s]", COMMANDS_API), e);
+        } catch (MalformedURLException e) {
+            throw new CommandsException(format("There was a problem with the url of the Commands API [commandsApi=%s]", COMMANDS_API), e);
+        } catch (JsonMappingException e) {
+            throw new CommandsException(format("There was a problem with json mapping for the Commands API response [commandsApi=%s]", COMMANDS_API), e);
+        } catch (IOException e) {
             throw new CommandsException(format("There was a problem with the Commands API [commandsApi=%s]", COMMANDS_API), e);
         }
     }
